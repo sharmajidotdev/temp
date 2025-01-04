aca9da14976d94d1d69d3bdd1a3fd2cded8f0420
Saurabh Sharma <saurabhsss1998@gmail.com>
2025-01-04 00:13:48 +0000

    Create aa.py

---
+++ aa.py
@@ -0,0 +1,43 @@
+
+# # # FUNCTIONS
+# # # collection of instructions
+
+# # def greetings():
+# #     print("Good morning Mineapple", end="")
+# #     print(", lookin very gud very nice")
+
+# # def add():
+# #     num1 = int(input("Enter number: "))
+# #     num2 = int(input("Enter number: "))
+# #     print(num1+num2)
+
+# # choice = int(input("Enter choice: "))
+
+
+# # if choice == 1:
+# #     add()
+# # elif choice == 2:
+# #     greetings()
+
+
+import math
+
+# INPUT
+num1 = int(input("num1:"))
+num2 = int(input("num2:"))
+
+## PROCESS
+catch = delme2.add2(num1, num2)
+
+## OUTPUT
+print(catch)
+
+
+
+
+
+
+
+
+
+
